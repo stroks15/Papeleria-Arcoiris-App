dac7c92859de6b33b1a61c32a8bb7dac899e187f
stroks15 <62426937+stroks15@users.noreply.github.com>
2026-09-06 13:47:45 -0600

    fix: corregir MainApplication.java para compatibilidad con RN actual

--- android/app/src/main/java/com/papeleriaarcoiris/app/MainApplication.java
+++ android/app/src/main/java/com/papeleriaarcoiris/app/MainApplication.java
@@ -1,36 +1,46 @@
 package com.papeleriaarcoiris.app;
 
 import android.app.Application;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactHost;
 import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultReactHost;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 
+import java.util.List;
+
 public class MainApplication extends Application implements ReactApplication {
+
     private final ReactNativeHost reactNativeHost = new DefaultReactNativeHost(this) {
         @Override
-        protected String getJSMainModuleName() { return "index"; }
-
-        @Override
-        protected boolean getUseDeveloperSupport() { return BuildConfig.DEBUG; }
-
-        @Override
-        protected boolean isNewArchEnabled() { return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED; }
-
-        @Override
-        protected Boolean isHermesEnabled() { return BuildConfig.IS_HERMES_ENABLED; }
-
-        @Override
-        protected java.util.List<com.facebook.react.ReactPackage> getPackages() {
+        protected List<ReactPackage> getPackages() {
             return new PackageList(this).getPackages();
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+
+        @Override
+        protected boolean isNewArchEnabled() {
+            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
         }
     };
 
     @Override
-    public ReactNativeHost getReactNativeHost() { return reactNativeHost; }
+    public ReactNativeHost getReactNativeHost() {
+        return reactNativeHost;
+    }
 
     @Override
-    public ReactHost getReactHost() { return DefaultReactHost.getDefaultReactHost(getApplicationContext(), reactNativeHost); }
+    public ReactHost getReactHost() {
+        return DefaultReactHost.getDefaultReactHost(
+                getApplicationContext(),
+                getReactNativeHost()
+        );
+    }
 }
