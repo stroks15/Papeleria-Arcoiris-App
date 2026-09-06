90f9bdc7d5eeea4230e1b868d7129b5b033c8bf3
stroks15 <62426937+stroks15@users.noreply.github.com>
2026-09-06 09:34:39 -0600

    refactor: move app navigation into dedicated navigator

--- App.tsx
+++ App.tsx
@@ -1,26 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/screens/Home';
-import FotoPDF from './src/screens/herramientas/FotoPDF/FotoPDF';
-import EscaneoPDF from './src/screens/herramientas/EscaneoPDF/EscaneoPDF';
-
-export type RootStackParamList = {
-  Home: undefined; FotoPDF: undefined; EscaneoPDF: undefined;
-  ComprimirPDF: undefined; RestaurarFotos: undefined; Cheatsheet: undefined;
-  AnuncioLocal: undefined; Flashcards: undefined; PedidoImpresion: undefined;
-  FotoCredencial: undefined; Recordatorios: undefined; Formatos: undefined; Investigacion: undefined;
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
-  return <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="FotoPDF" component={FotoPDF} />
-      <Stack.Screen name="EscaneoPDF" component={EscaneoPDF} />
-    </Stack.Navigator>
-  </NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
