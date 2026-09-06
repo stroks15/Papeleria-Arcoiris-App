89b0514387635410d08e9364b822fee5e53766b2
stroks15 <62426937+stroks15@users.noreply.github.com>
2026-09-06 15:20:31 -0600

    fix: eliminar OpenSourceMergedSoMapping incompatible

--- android/app/src/main/java/com/papeleriaarcoiris/app/MainApplication.java
+++ android/app/src/main/java/com/papeleriaarcoiris/app/MainApplication.java
@@ -10,7 +10,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactHost;
 import com.facebook.react.defaults.DefaultReactNativeHost;
-import com.facebook.react.soloader.OpenSourceMergedSoMapping;
 import com.facebook.soloader.SoLoader;
 
 import java.util.List;
@@ -61,7 +60,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        SoLoader.init(this, OpenSourceMergedSoMapping);
+        SoLoader.init(this, false);
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             DefaultNewArchitectureEntryPoint.load();
         }
